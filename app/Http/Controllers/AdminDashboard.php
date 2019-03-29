103953d3586cbac0f216fc0a93b62ae1d5c7139c
assem abozaid <assemabozaidfcih@gmail.com>
2019-03-30 00:22:32 +0200

    Add Members

--- app/Http/Controllers/AdminDashboard.php
+++ app/Http/Controllers/AdminDashboard.php
@@ -27,7 +27,22 @@ class AdminDashboard extends Controller
         return view('dashboards.admin.users.show');
     }
 
-    public function addMembers() {
+    public function addMembers(Request $request) {
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:admins',
+            'password' => 'required|min:6|confirmed',
+        ]);
 
+        $admin = new Admin();
+        $admin->name = $request->input('name');
+        $admin->email = $request->input('email');
+        $admin->password = bcrypt($request->input('password'));
+
+        if($admin->save())
+            return redirect('dashboard/admin/addmember')->withSuccess('Member has been added');
+        else
+            return redirect('dashboard/admin/addmember')->withDanger('Something wrong has happened');
+        
     }
 }
