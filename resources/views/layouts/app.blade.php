388e096ef9dabaa9c33549fbd985ef88f842b69c
sa3d clay <sa3d.clay@mail.com>
2019-10-13 14:44:37 +0200

    few updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,11 +23,13 @@
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 
     <!-- Bootstrap -->
+    {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap3.2.0cssbootstrap.min.css') }}"> --}}
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
     <!-- Font Awesome -->
+    {{-- <link rel="stylesheet" href="{{ asset('css/fontawesome.comreleasesv5.7.2cssall.css') }}"> --}}
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"> 
-
+    
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
